bed76f7ce20afb39da6b1f9fd810223c125532b8
Spencer Schurk <spencer.schurk@gmail.com>
2020-01-21 14:11:54 -0800

    Added new base layer for mac, mic mute macro, moved arrow keys to wasd, fixed tilda

--- keyboards/keebio/quefrency/keymaps/rocketeer55/keymap.c
+++ keyboards/keebio/quefrency/keymaps/rocketeer55/keymap.c
@@ -7,11 +7,29 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _BASE 0
-#define _FN1 1
-#define _FN2 2
+#define _MAC 1
+#define _FN1 2
+
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
+  MUTE_MIC = SAFE_RANGE,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch(keycode) {
+        case MUTE_MIC:
+        if (record->event.pressed) {
+            // when keycode MUTE_MIC is pressed
+            register_code(KC_LCMD);
+            register_code(KC_LSHIFT);
+            register_code(KC_0);
+        } else {
+            // when keycode MUTE_MIC is released
+            clear_keyboard();
+        }
+        break;
+    }
+    return true;
 };
 
 
@@ -37,15 +55,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, _______, \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  MO(_FN1),         KC_SPC,  _______, MO(_FN1),KC_RALT, _______, KC_RGUI, KC_RCTL
+    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  MO(_FN1),         KC_SPC,  _______, TG(_MAC),KC_RALT, _______, KC_RGUI, KC_RCTL
+  ),
+
+  [_MAC] = LAYOUT(
+    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  _______,  KC_BSPC, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
+    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, _______, \
+    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  MO(_FN1),         KC_SPC,  _______, _______,  KC_RGUI, _______, KC_RALT, KC_RCTL
   ),
 
   [_FN1] = LAYOUT(
-    KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, \
-    RGB_TOG, RGB_MOD, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______,\
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    KC_TILD, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______
+    KC_GRAVE, KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,     KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, \
+    RGB_TOG, RGB_MOD, KC_UP,   _______, _______, _______, _______, _______,  _______, _______, KC_PSCREEN, _______, _______, _______, \
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______,  _______, _______, _______,    _______, _______,\
+    _______, _______, _______, _______, _______, _______, _______, MUTE_MIC, _______, _______, _______,    _______, _______, \
+    _______, _______, _______, _______, _______,          _______, _______,  _______, _______, _______,    _______, _______
   )
 };
 
